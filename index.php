4b157316b21c69d0ce8e764edb8ae872bf9faa46
Lourdes78 <60180862+Lourdes78@users.noreply.github.com>
2021-12-10 09:48:57 +0100

    master

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "views/header.php";
+?>
+
+<div class="container mt-5">
+  <div class="row">
+
+    </div>
+    <div class="col-sm-8">
+     
+    </div>
+  </div>
+</div>
+<?php
+include "views/footer.php";
+?>
+
+</body>
+</html>
