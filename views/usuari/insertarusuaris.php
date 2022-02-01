215a9397f20ca0d640455f6bfa248ddc0b44eb41
Lourdes78 <60180862+Lourdes78@users.noreply.github.com>
2022-02-01 19:11:19 +0100

    master

--- views/usuari/insertarusuaris.php
+++ views/usuari/insertarusuaris.php
@@ -19,7 +19,7 @@
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-      <form action="../../index.php?controller=usuari&action=guardarusuaris" method="post">
+      <form action="../../index.php?controller=usuari&action=guardarusuari" method="post">
         <div class="card bg-dark text-white" style="border-radius: 1rem;">
           <div class="card-body p-5 ">
 
